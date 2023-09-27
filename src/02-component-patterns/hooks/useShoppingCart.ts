43edcab410c05029aa96682a6e22cf1641b28961
Javier Carmona <javier.carmona@wizeline.com>
2023-09-27 16:50:41 -0600

    [FEAT] Component control props alt ready

--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -11,31 +11,31 @@ export const useShoppingCart = () => {
 
         setShoppingCart(oldShoppingCart => {
 
-            const productInCart: ProductInCart = oldShoppingCart[product.id] || { ...product, count: 0 };
+            // const productInCart: ProductInCart = oldShoppingCart[product.id] || { ...product, count: 0 };
 
-            if (Math.max(productInCart.count + count, 0) > 0) {
-                productInCart.count += count;
-                return {
-                    ...oldShoppingCart,
-                    [product.id]: productInCart
-                }
-            }
+            // if (Math.max(productInCart.count + count, 0) > 0) {
+            //     productInCart.count += count;
+            //     return {
+            //         ...oldShoppingCart,
+            //         [product.id]: productInCart
+            //     }
+            // }
 
             //Borrar  el producto
             
-            const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-            return rest
+            // const { [product.id]: toDelete, ...rest } = oldShoppingCart;
+            // return rest
 
-            // if (count === 0) {
-            //     const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-            //     console.log({ toDelete });
-            //     return { ...rest }
-            // }
+            if (count === 0) {
+                const { [product.id]: toDelete, ...rest } = oldShoppingCart;
+                console.log({ toDelete });
+                return { ...rest }
+            }
 
-            // return {
-            //     ...oldShoppingCart,
-            //     [product.id]: { ...product, count }
-            // }
+            return {
+                ...oldShoppingCart,
+                [product.id]: { ...product, count }
+            }
 
         })
     }
